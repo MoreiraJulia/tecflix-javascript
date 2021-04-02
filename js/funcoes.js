484ee36aa88615455177e5bc70879f241d36aefa
Júlia Aparecida <julia.aparecida25@gmail.com>
2021-04-01 22:28:40 -0300

    Atualização do JavaScript

---
+++ js/funcoes.js
@@ -0,0 +1,13 @@
+let series = ["Grey's Anatomy" , "Por trás dos seus olhos" , "As telefonistas"];
+console.log(series);
+console.log(series.length);
+
+// console.log(series[0]);
+// console.log(series[1]);
+// console.log(series[2]);
+
+let quantidadeSeries = 3;
+while(quantidadeSeries >= 0){
+    console.log(series[quantidadeSeries]);
+    quantidadeSeries--;
+}
